136e58fc43f66e07adca154e05df25ecedc7a7fa
raymondngiam <ha.ngiam87@gmail.com>
2021-01-04 14:39:17 +0000

    Edit welcome message

--- script/welcome_message.cpp
+++ script/welcome_message.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginMyRobot() : WorldPlugin()
             {
-              printf("Welcome to Gazebo’s World\n");
+              printf("Welcome to Raymond’s World\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
